a22f45bdd3a6c944c40380d270406c2d27fe0437
Achim Fritz <achim.fritz@b13.de>
2022-04-27 08:32:52 +0200

    [BUGFIX] prevent deprecated object instantiation

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 call_user_func(
     function () {
-        $rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
+        $rendererRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::class);
         $rendererRegistry->registerRendererClass(\B13\Twoclickmedia\Resource\Rendering\YouTubeRenderer::class);
         $rendererRegistry->registerRendererClass(\B13\Twoclickmedia\Resource\Rendering\VimeoRenderer::class);
     }
