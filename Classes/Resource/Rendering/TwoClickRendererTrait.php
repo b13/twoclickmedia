7e4acfcad4620a1ce7f4709aa20462a94cfa10d8
Johannes Schlier <johannes.schlier@b13.com>
2026-02-12 10:57:07 +0100

    [TASK] Refactor renderer code and template rendering for TYPO3 13+
    The StandaloneView was deprecated with TYPO3 13. Using the new way to generate the templates means we drop TYPO3 12 compatibility.
    This commit also introduces usage of a PHP trait to keep us from having duplicate code.

---
+++ Classes/Resource/Rendering/TwoClickRendererTrait.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace B13\Twoclickmedia\Resource\Rendering;
+
+/*
+ * This file is part of TYPO3 CMS-based extension "twoclickmedia" by b13.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ */
+
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use TYPO3\CMS\Core\Resource\FileInterface;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\View\ViewFactoryData;
+use TYPO3\CMS\Core\View\ViewFactoryInterface;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+
+#[Autoconfigure(public: true)]
+trait TwoClickRendererTrait
+{
+    public function __construct(
+        protected readonly ConfigurationManager $configurationManager,
+        protected readonly ViewFactoryInterface $viewFactory,
+    ) {}
+
+    public function render(
+        FileInterface $file,
+        $width,
+        $height,
+        array $options = []
+    ) {
+        $options = $this->collectOptions($options, $file);
+        $src = $this->createVideoUrl($options, $file);
+        $attributes = $this->collectIframeAttributes($width, $height, $options);
+
+        $extensionConfiguration = $this->configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_FRAMEWORK, 'Twoclickmedia');
+
+        if (!($extensionConfiguration['settings']['mediaSecure'] ?? false)) {
+            return parent::render($file, $width, $height, $options);
+        }
+
+        $variables = [
+            'file' => $file,
+            'src' => $src,
+            'type' => self::type,
+            'isReference' => $file instanceof FileReference,
+            'dimensions' => ['width' => $width, 'height' => $height],
+            'attributes' => empty($attributes) ? '' : ' ' . $this->implodeAttributes($attributes),
+        ];
+
+        // calculate the padding for the item
+        if (!empty($file->getProperty('height')) && !empty($file->getProperty('width'))) {
+            $paddingTop = ((int)$file->getProperty('height') / (int)$file->getProperty('width')) * 100;
+            $variables['paddingTop'] = $paddingTop;
+        }
+
+        $viewFactoryData = new ViewFactoryData(
+            templateRootPaths: $extensionConfiguration['view']['templateRootPaths'],
+            partialRootPaths: $extensionConfiguration['view']['partialRootPaths'],
+            layoutRootPaths: $extensionConfiguration['view']['layoutRootPaths'],
+            request: $GLOBALS['TYPO3_REQUEST'],
+        );
+        $view = $this->viewFactory->create($viewFactoryData);
+        $view->assignMultiple($variables);
+        return $view->render(self::templateName);
+    }
+
+    abstract function createVideoUrl(array $options, FileInterface $file);
+}
--- Classes/Resource/Rendering/YouTubeRenderer.php
+++ Classes/Resource/Rendering/YouTubeRenderer.php
@@ -20,18 +20,10 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
 
 class YouTubeRenderer extends \TYPO3\CMS\Core\Resource\Rendering\YouTubeRenderer
 {
+    use TwoClickRendererTrait;
+
     public const templateName = 'YouTube';
     public const type = 'youtube';
-
-    /**
-     * @var ConfigurationManager
-     */
-    protected $configurationManager;
-
-    public function injectConfigurationManager(ConfigurationManager $configurationManager): void
-    {
-        $this->configurationManager = $configurationManager;
-    }
 
     /**
      * @return int
@@ -41,51 +33,8 @@ class YouTubeRenderer extends \TYPO3\CMS\Core\Resource\Rendering\YouTubeRenderer
         return 50;
     }
 
-    /**
-     * @param FileInterface $file
-     * @param int|string $width
-     * @param int|string $height
-     * @param array $options
-     * @return string
-     */
-    public function render(
-        FileInterface $file,
-        $width,
-        $height,
-        array $options = []
-    ) {
-        $options = $this->collectOptions($options, $file);
-        $src = $this->createYouTubeUrl($options, $file);
-        $attributes = $this->collectIframeAttributes($width, $height, $options);
-
-        $extensionConfiguration = $this->configurationManager->getConfiguration(ConfigurationManager::CONFIGURATION_TYPE_FRAMEWORK, 'Twoclickmedia');
-
-        if (!($extensionConfiguration['settings']['mediaSecure'] ?? false)) {
-            return parent::render($file, $width, $height, $options);
-        }
-
-        $variables = [
-            'file' => $file,
-            'src' => $src,
-            'type' => self::type,
-            'isReference' => $file instanceof FileReference,
-            'dimensions' => ['width' => $width, 'height' => $height],
-            'attributes' => empty($attributes) ? '' : ' ' . $this->implodeAttributes($attributes),
-        ];
-
-        // calculate the padding for the item
-        if (!empty($file->getProperty('height')) && !empty($file->getProperty('width'))) {
-            $paddingTop = ((int)$file->getProperty('height') / (int)$file->getProperty('width')) * 100;
-            $variables['paddingTop'] = $paddingTop;
-        }
-
-        $view = GeneralUtility::makeInstance(StandaloneView::class);
-        $view->setLayoutRootPaths($extensionConfiguration['view']['layoutRootPaths']);
-        $view->setPartialRootPaths($extensionConfiguration['view']['partialRootPaths']);
-        $view->setTemplateRootPaths($extensionConfiguration['view']['templateRootPaths']);
-        $view->setTemplate(self::templateName);
-        $view->assignMultiple($variables);
-        $view->setRequest($GLOBALS['TYPO3_REQUEST']);
-        return $view->render();
+    public function createVideoUrl(array $options, FileInterface $file)
+    {
+        return $this->createYouTubeUrl($options, $file);
     }
 }
